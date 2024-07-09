d4dc4a28c20158ba428b45457e1e0b0f843b983b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-10 05:28:32 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navbar from "./components/navbar";
 
 export default function App() {
   return (
     <div className="">
-      <Navbar />
       <BrowserRouter>
         <Routes>
           <Route />
