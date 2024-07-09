09db787a05e0f45d6162ffa3dff47ce86d34941e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-10 05:28:45 +0530

    Update App.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
 
 export default function App() {
   return (
     <div className="">
+      <Nav />
       <BrowserRouter>
         <Routes>
           <Route />
