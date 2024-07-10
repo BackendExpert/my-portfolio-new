75d2f2ac9a14ae632c2add490fb020a0f554fafa
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-10 06:53:24 +0530

    Update Nav.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import MyImg from '../assets/file.jpg'
+import { BsList, BsX } from 'react-icons/bs'
 
 const Nav = () => {
     const [isTop, setIsTop] = useState(true);
@@ -19,14 +20,28 @@ const Nav = () => {
         window.removeEventListener('scroll', handleScroll);
       };
     }, []);
+
+    const [navOpen, SetnapOpen] = useState(false)
+
   return (
-    <nav className={`fixed top-0 left-0 w-full p-4 transition-colors duration-300 ${isTop ? '' : 'bg-gray-900'}`}>
+    <nav className={`fixed top-0 left-0 w-full p-4 transition-colors duration-500 ${isTop ? '' : 'bg-red-900 h-16'}`}>
       <div className="container mx-auto flex justify-between">
         <div className="flex">
-            <img src={MyImg} alt="" className='h-12 w-auto rounded-full'/>
-            <p className="text-white pl-2 py-3 text-xl">Jehan Weerasuriya</p>
+            <img src={MyImg} alt="" className={`w-auto rounded-full ${isTop ? 'h-12' : 'h-8' }`}/>
+            <p className={`text-white ${isTop ? 'pl-2 py-3 text-xl' : 'pl-2 py-1 text-xl'}` }>Jehan Weerasuriya</p>
         </div>
-        <div className="text-white py-3">
+
+        <div className="md:hidden block text-white">
+            {
+                navOpen ? 
+                    <BsList  className='h-8 w-auto mt-3'/>   
+                :
+                    <BsX  className='h-8 w-auto mt-3'/>   
+
+            }                     
+        </div>
+
+        <div className={`text-white ${isTop ? 'py-3' : 'py-[6px]'} ${navOpen ? '' : 'hidden'}`}>
             <div className="flex ">
                 <a href="#home"><p className="px-3">Home</p></a>
                 <a href="#Content"><p className="px-3">Link 1</p></a>
