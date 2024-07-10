ed60e9d550e0b7dda996421d8e796de7e6545275
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-10 06:26:42 +0530

    Update Nav.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -26,6 +26,15 @@ const Nav = () => {
             <img src={MyImg} alt="" className='h-12 w-auto rounded-full'/>
             <p className="text-white pl-2 py-3 text-xl">Jehan Weerasuriya</p>
         </div>
+        <div className="text-white py-3">
+            <div className="flex ">
+                <a href=""><p className="px-3">Link 1</p></a>
+                <a href=""><p className="px-3">Link 1</p></a>
+                <a href=""><p className="px-3">Link 1</p></a>
+                <a href=""><p className="px-3">Link 1</p></a>
+                <a href=""><p className="px-3">Link 1</p></a>
+            </div>
+        </div>
       </div>
     </nav>
   )
