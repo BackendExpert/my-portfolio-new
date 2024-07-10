8ed07ac9bc5287755eb5bc0adedf7a46d412dd55
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-10 05:37:35 +0530

    Update Home.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-        <div className="flex"></div>
+        <div className="flex">
+            <div className=""></div>
+            <div className="">
+                
+            </div>
+        </div>
     </div>
   )
 }
