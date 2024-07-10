c549124f0af5f22f635dfdb27f4e9886ff461143
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-10 06:24:52 +0530

    Update Nav.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -21,8 +21,11 @@ const Nav = () => {
     }, []);
   return (
     <nav className={`fixed top-0 left-0 w-full p-4 transition-colors duration-300 ${isTop ? '' : 'bg-gray-900'}`}>
-      <div className="container mx-auto">
-        <h1 className="text-white">My Navbar</h1>
+      <div className="container mx-auto flex justify-between">
+        <div className="flex">
+            <img src={MyImg} alt="" className='h-12 w-auto rounded-full'/>
+            <p className="text-white pl-2 py-3 text-xl">Jehan Weerasuriya</p>
+        </div>
       </div>
     </nav>
   )
