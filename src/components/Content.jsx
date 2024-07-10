f5968eb4d84c86e6bd0c603743ee5523c389d7d2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-10 06:17:14 +0530

    Create Content.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ src/components/Content.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Content = () => {
+  return (
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, eligendi obcaecati debitis repudiandae earum quo animi. Quibusdam expedita accusamus vero pariatur. Eveniet soluta quia molestias quaerat libero voluptate magni porro.</div>
+  )
+}
+
+export default Content
