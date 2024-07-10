18d7fb2e0a9da3c2d6d13edd8825eeb0a16ff45a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-10 07:48:58 +0530

    Update Nav.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -23,31 +23,35 @@ const Nav = () => {
 
     const [navOpen, SetnapOpen] = useState(false)
 
+    const headleNav = () => {
+        SetnapOpen(!navOpen)
+    } 
+
   return (
-    <nav className={`fixed top-0 left-0 w-full p-4 transition-colors duration-500 ${isTop ? '' : 'bg-red-900 h-16'}`}>
+    <nav className={`fixed top-0 left-0 w-full transition-colors duration-500 ${isTop ? '' : 'bg-red-900 h-16'}`}>
       <div className="container mx-auto flex justify-between">
-        <div className="flex">
+        <div className="flex p-4">
             <img src={MyImg} alt="" className={`w-auto rounded-full ${isTop ? 'h-12' : 'h-8' }`}/>
             <p className={`text-white ${isTop ? 'pl-2 py-3 text-xl' : 'pl-2 py-1 text-xl'}` }>Jehan Weerasuriya</p>
         </div>
 
-        <div className="md:hidden block text-white">
+        <div className={`md:hidden block text-white p-4`} onClick={headleNav}>
             {
-                navOpen ? 
-                    <BsList  className='h-8 w-auto mt-3' />   
+                !navOpen ? 
+                    <BsList  className={`${isTop ? 'h-10 w-auto pt-2' : 'h-8  w-auto'}`} />   
                 :
-                    <BsX  className='h-8 w-auto mt-3' />   
+                    <BsX  className={`${isTop ? 'h-10 w-auto pt-2' : 'h-8  w-auto'}`} />   
 
             }                     
         </div>
 
-        <div className={`text-white ${isTop ? 'py-3' : 'py-[6px]'} ${navOpen ? '' : 'hidden'}`}>
-            <div className="flex ">
-                <a href="#home"><p className="px-3">Home</p></a>
-                <a href="#Content"><p className="px-3">Link 1</p></a>
-                <a href=""><p className="px-3">Link 1</p></a>
-                <a href=""><p className="px-3">Link 1</p></a>
-                <a href=""><p className="px-3">Link 1</p></a>
+        <div className={`text-white md:relative fixed md:bg-none md:backdrop-blur-none backdrop-blur-xl md:bg-none bg-white/10 md:w-auto w-full md:pt-6 pt-0  ${isTop ? 'md:py-3 md:mt-0 mt-20' : 'md:py-[6px] '} transform ${navOpen ? 'translate-y-0 md:my-0 my-16' : '-translate-y-full md:my-0 my-[-40px]'} transition-transform duration-300 ease-in-out md:relative md:bg-transparent md:transform-none`}>
+            <div className="md:flex w-full">
+                <a href="#home"><p className="px-3 md:py-0 py-4">Home</p></a>
+                <a href="#Content"><p className="px-3 md:py-0 py-4">Link 1</p></a>
+                <a href=""><p className="px-3 md:py-0 py-4">Link 1</p></a>
+                <a href=""><p className="px-3 md:py-0 py-4">Link 1</p></a>
+                <a href=""><p className="px-3 md:py-0 py-4">Link 1</p></a>
             </div>
         </div>
       </div>
