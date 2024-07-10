76bae00a789ded0bb31ec75635210848af0e6900
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-10 05:56:33 +0530

    Update Home.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
+import MyImg from '../assets/file.jpg'
 
 const Home = () => {
+    const styles = {
+        background: `linear-gradient(to right, #1F2937, rgba(0,0,0,0.5)), url(${MyImg})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+    }
   return (
     <div>
-        <div className="flex">
-            <div className=""></div>
-            <div className="">
+        <div className="md:grid grid-cols-2 gap-0">
+            <div className="bg-gray-800"></div>
+            <div className="md:block hidden py-10 px-6 h-screen" style={styles}>
                 
             </div>
         </div>
     </div>
+
   )
 }
 
