4d0f2dbaa0a515d46b37374a4516ea1175d4ac94
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-14 18:32:03 +0530

    UPdate
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Nav from './Nav'
 
 const Home = () => {
   return (
     <div className="bg-gradient-to-br from-purple-600 to-blue-600 min-h-screen md:px-40 px-4">
-
+        <Nav />
     </div>
 
   )
