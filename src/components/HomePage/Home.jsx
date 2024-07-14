fe114a5f03c1af4655a92276c43d528cf4cac039
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-14 18:27:04 +0530

    Update Home.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className="container">
-            <div>Home</div>
+    <div className="container mx-auto">
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero odit aliquam placeat tempore, atque accusantium quis non deleniti saepe, deserunt repellendus ad. Corporis odit neque magni voluptate provident sed? Incidunt!
+        </div>
     </div>
   )
 }
