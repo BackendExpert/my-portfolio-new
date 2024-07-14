f3f6e4cfd6494e2f25b503e4a4ffd6641a034e44
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-14 18:37:47 +0530

    Update Nav.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/HomePage/Nav.jsx
+++ src/components/HomePage/Nav.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import MyImg from '../../assets/file.jpg'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div className="pt-6">
+        <div className='bg-white py-4 px-8 rounded shadow-md'>
+            <div className="flex">
+                <img src={MyImg} alt="" className='h-8 w-auto rounded-full'/>
+                <h1 className="text-xl pt-1 pl-2 font-semibold text-gray-500">Jehan Weerasuriya</h1>
+            </div>
+        </div>
+    </div>
   )
 }
 
