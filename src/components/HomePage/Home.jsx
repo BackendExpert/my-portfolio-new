49def0c894cac14197069d8aa7341ee81783131d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-14 18:31:38 +0530

    Update Home.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className="container mx-auto">
-        <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero odit aliquam placeat tempore, atque accusantium quis non deleniti saepe, deserunt repellendus ad. Corporis odit neque magni voluptate provident sed? Incidunt!
-        </div>
+    <div className="bg-gradient-to-br from-purple-600 to-blue-600 min-h-screen md:px-40 px-4">
+
     </div>
+
   )
 }
 
